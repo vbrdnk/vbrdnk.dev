56c16a116e25646ee32bf76aa1b769516c9094e1
vbrdnk <burdenyuchok@gmail.com>
2021-10-27 15:18:28 +0300

    chore: bump Next.js version to 12.0.1

--- src/pages/api/now-playing.ts
+++ src/pages/api/now-playing.ts
@@ -21,8 +21,6 @@ export default async function handler(_req: NextApiRequest, res: NextApiResponse
   const albumImageUrl = song.item.album.images[0].url;
   const songUrl = song.item.external_urls.spotify;
 
-  res.setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=30');
-
   return res.status(200).json({
     album,
     albumImageUrl,
