b58d59076c9be2e0bd77474790c92a1a10e6ece5
vbrdnk <vladyslav.burdeniuk@allsetnow.com>
2022-03-28 17:02:23 -0400

    fix: prevent incorrect event bubbling on nav item

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import Link from 'next/link';
-import { Stack, Button } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
+import { Stack } from '@chakra-ui/react';
+
+import NavItem from '@/components/NavItem';
 
 const Nav: React.FC = () => {
-  const router = useRouter();
   return (
     <Stack direction="row" spacing={1} align="center" as="nav">
-      <Button variant="ghost" isActive={router.asPath === '/'}>
-        <Link href="/">Home</Link>
-      </Button>
-      <Button variant="ghost" isActive={router.asPath === '/dashboard'}>
-        <Link href="/dashboard">Dashboard</Link>
-      </Button>
-      <Button variant="ghost" isActive={router.asPath === '/blog'}>
-        <Link href="/blog">Blog</Link>
-      </Button>
+      <NavItem href="/" text="Home" />
+      <NavItem href="/dashboard" text="Dashboard" />
+      <NavItem href="/blog" text="Blog" />
     </Stack>
   );
 };
---
+++ src/components/NavItem/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Button } from '@chakra-ui/react';
+
+type NavItemProps = {
+  href: string;
+  text: string;
+};
+
+const NavItem: React.FC<NavItemProps> = ({ href, text }) => {
+  const router = useRouter();
+  const isActive = router.asPath === href;
+
+  return (
+    <Link href={href} passHref>
+      <Button as="a" variant="ghost" isActive={isActive}>
+        {text}
+      </Button>
+    </Link>
+  );
+};
+
+export default NavItem;
