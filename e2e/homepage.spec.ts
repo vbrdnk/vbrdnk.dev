4256b1021d8d5db84423ce03c494f80ee2014856
Vladyslav Burdeniuk <vladyslavb@spotify.com>
2023-04-07 11:12:24 -0400

    Update e2e tests
    Update e2e tests

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach(async ({page}) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/');
 });
 
@@ -9,19 +9,15 @@ test('homepage has proper title and navigation links', async ({ page }) => {
 
   // check nav has correct nav
   const navLinks = page.locator('nav button.chakra-button');
-  await expect(navLinks).toHaveCount(3);
-  await expect(navLinks).toHaveText(['Home', 'Dashboard', 'Blog']);
+  await expect(navLinks).toHaveCount(2);
+  await expect(navLinks).toHaveText(['Home', 'Blog']);
 
   // check all nav links are correct
   const homeLink = page.locator('button.chakra-button a', { hasText: 'Home' });
-  const dashboardLink = page.locator('button.chakra-button a', { hasText: 'Dashboard' });
   const blogLink = page.locator('button.chakra-button a', { hasText: 'Blog' });
 
   await expect(homeLink).toHaveAttribute('href', '/');
-  await expect(dashboardLink).toHaveAttribute('href', '/dashboard');
   await expect(blogLink).toHaveAttribute('href', '/blog');
-  await dashboardLink.click();
-  await expect(page).toHaveURL(/dashboard/);
   await blogLink.click();
   await expect(page).toHaveURL(/blog/);
 });
@@ -29,5 +25,5 @@ test('homepage has proper title and navigation links', async ({ page }) => {
 test('homepage has correct headings', async ({ page }) => {
   const homePageHeadings = page.locator('h2.chakra-heading');
   await expect(homePageHeadings).toHaveCount(2);
-  await expect(homePageHeadings).toHaveText(['Blog', 'Dashboard']);
+  await expect(homePageHeadings).toHaveText(['Blog', 'Work playlist']);
 });
